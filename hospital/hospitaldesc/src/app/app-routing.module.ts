1e04493f5b83697784b13c7b800f2904e6a9cd7b
yashu <rangaswamy12222@gmail.com>
2023-01-10 15:50:15 +0530

    hi

--- hospital/hospitaldesc/src/app/app-routing.module.ts
+++ hospital/hospitaldesc/src/app/app-routing.module.ts
@@ -1,18 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AddDoctorComponent } from './dashboard/doctor/add-doctor/add-doctor.component';
-import { DoctorComponent } from './dashboard/doctor/doctor.component';
-import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [
-  {path:'',component:LoginComponent},
-  {path:'doctor',component:DoctorComponent, children:[
-    {path:'add-doctor',component:AddDoctorComponent}
-  ]}
-  
-  
-=======
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DoctorComponent } from './dashboard/doctor/doctor.component';
 import { PatientComponent } from './dashboard/patient/patient.component';
@@ -43,7 +30,6 @@ const routes: Routes = [
   }
 
 
->>>>>>> a92fe91bb3130f7b5e099eba140f9b47f3104460
 ];
 
 @NgModule({
