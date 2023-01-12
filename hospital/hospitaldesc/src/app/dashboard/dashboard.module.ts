ff5922c17080612368c33e8aedce803e23ad0402
vinodhraj bp <vinodhrajbp1999@gmail.com>
2023-01-12 14:35:17 +0530

    add doctors created raj

--- hospital/hospitaldesc/src/app/dashboard/dashboard.module.ts
+++ hospital/hospitaldesc/src/app/dashboard/dashboard.module.ts
@@ -19,35 +19,14 @@ import { DoctorDetailsComponent } from './doctor/doctor-details/doctor-details.c
     PatientComponent,
     SildebarComponent,
     AddDoctorComponent,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
     AppointmentsComponent,
     DoctorDetailsComponent
  
-||||||| 3e94afe
-    AppointmentsComponent
+ 
 
 
 
-
-=======
-<<<<<<< HEAD
-   
-
-  
-=======
->>>>>>> a2931184fc721797560ed59209d47a65b15c67f6
-    AppointmentsComponent
-
-
-
-
-<<<<<<< HEAD
-=======
->>>>>>> 3e94afe70e86c177d65e85711ffc8241793d067a
->>>>>>> 7d886be9e01ccb439c86e035257d3b4e85e5d0e1
->>>>>>> a2931184fc721797560ed59209d47a65b15c67f6
   ],
   imports: [
     CommonModule,
