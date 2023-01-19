c0d23db88834ca4607279225fc09a58bba52f74d
yashu <rangaswamy12222@gmail.com>
2023-01-19 12:09:54 +0530

    yashu

--- hospital/hospitaldesc/src/app/dashboard/patient/add-patient.component.ts/add-patient.component.ts
+++ hospital/hospitaldesc/src/app/dashboard/patient/add-patient.component.ts/add-patient.component.ts
@@ -31,15 +31,14 @@ export class AddPatientComponent implements OnInit {
       firstName: new FormControl('',[Validators.required]),
       lastName: new FormControl('',[Validators.required]),
       age: new FormControl('',[Validators.required]),
-      ph: new FormControl("",[Validators.required, Validators.pattern("^[0-9]{10}$"),Validators.maxLength(10)
-    ]),
-      gender: new FormControl(''),
+      ph: new FormControl('',[Validators.required]),
+      // gender: new FormControl(''),
       email: new FormControl('',[Validators.email,Validators.required]),
       address: new FormControl('',[Validators.required]),
       doctorName: new FormControl('',[Validators.required]),
       specilist: new FormControl('',[Validators.required]),
-      date: new FormControl(''),
-      time: new FormControl(''),
+      // date: new FormControl(''),
+      // time: new FormControl(''),
       doctorEmail: new FormControl('',[Validators.email])
   
     })
@@ -56,7 +55,7 @@ export class AddPatientComponent implements OnInit {
         this.route.navigate(['../dashboard']);
     }
     onSub() {
-      console.log(this.register.value.date);
+      // console.log(this.register.value.date);
       let a=this.register.value
       // let patientData={name:a.firstName!+a.lastName,age:a.age,gender:a.gender,ph:a.ph,email:a.email,address:a.address}
       // let doctorData={name:a.doctorName,speciliat:a.specilist,appointmentDate:a.date,appointmentTime:a.time,email:a.doctorEmail}
@@ -91,7 +90,6 @@ export class AddPatientComponent implements OnInit {
        }
        return isTimeOver;
        }
-      
       
   }
   
